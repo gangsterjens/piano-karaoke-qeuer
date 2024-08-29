01b6349ae34435f1604a5d032064ffd69efbe462
Øystein Skjellfjord <90387893+gangsterjens@users.noreply.github.com>
2024-08-29 19:29:43 +0200

    Update main.py

--- main.py
+++ main.py
@@ -48,38 +48,20 @@ with t1:
 # Initialize a session state variable to track which form to show
 
 with t2:
-    songs = supabase.table("song_list").select("*").execute()
-    if "form_key" not in st.session_state:
-        st.session_state.form_key = None
+    test = supabase.table("song_list").select("*").execute()
     
-    # Loop through each song in the fetched data
-    for el in songs.data:
+    for el in test.data:
         col1, col2 = st.columns(2)
-        
-        # Display artist and song information
-        col1.markdown(f" ### {el['artist']} - {el['song']}")
-        
-        unique_key = str(uuid.uuid4())  # Generate a unique key for each form
-        
-        # When the button is clicked, set the session state to the current form's unique key
-        if col2.button('Velg', key='button' + unique_key):
-            st.session_state.form_key = unique_key
-        
-        # Only display the form if its unique key matches the session state's form_key
-        if st.session_state.form_key == unique_key:
+        col1.markdown(f" ### {el['artist']} {el['song']}")
+        unique_key = str(uuid.uuid4())
+        if col2.button('Velg', key='button'+unique_key):
             with st.form(unique_key):
                 form_name = st.text_input("Hva er navnet ditt?")
-                if st.form_submit_button("Send inn!"):
-                    current_time = datetime.now().isoformat()
-                    supabase.table("qeuer").insert({
-                        "uuid": unique_key,
-                        "name": form_name,
-                        "song": el['song'],
-                        "artist": el['artist'],
-                        "created_at": current_time
-                    }).execute()
-                    
-                    st.success('Nydelig! Du vil bli ropt opp når det er din tur!', icon="✅")
-        
-        # Add a separator
+                st.form_submit_button("Send inn!")
+                supabase.table("qeuer").insert({"uuid": user_uuid, 
+                                            "name": form_name,
+                                            "song": el['song'],
+                                            "artist": el['artist'],
+                                            "created_at": current_time}).execute()
+                st.success('Nydelig! Du vil bli ropt opp når det er din tur!', icon="✅")
         st.markdown("<hr>", unsafe_allow_html=True)
