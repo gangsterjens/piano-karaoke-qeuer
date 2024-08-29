7b1d00285b27bc4a54f7969a3aa2d0def4f0c050
Øystein Skjellfjord <90387893+gangsterjens@users.noreply.github.com>
2024-08-29 18:45:48 +0200

    Update main.py

--- main.py
+++ main.py
@@ -3,53 +3,67 @@ from supabase import create_client, Client
 import uuid
 import datetime 
 
-user_uuid = str(uuid.uuid4())
-
-st.markdown("# Karaoke-kø")
-
-# Initialize connection.
-# Uses st.cache_resource to only run once.
-@st.cache_resource
-def init_connection():
-    url = st.secrets["API_URL"]
-    key = st.secrets["API_KEY"]
-    return create_client(url, key)
-
-supabase = init_connection()
-
-#test = supabase.table("qeuer").select("*").execute()
-st.markdown("Skjema for sangforespørsel")
-
-name = st.text_input("Hva er navnet ditt?")
-song = st.text_input("Hvilken sang vil du spille?")
-artist = st.text_input("Hva heter artisten?")
-
-if st.button("Send inn!"):
-    if len(name) == 0:
-        st.error("Vennligst skriv navnet ditt")
+t1, t2 = st.tabs(2)
+t1.write("Skjema")
+t2.write("liste over sanger")
+with t1:
+    user_uuid = str(uuid.uuid4())
     
-    elif len(song) == 0:
-        st.error("Sang mangler")
+    st.markdown("# Karaoke-kø")
     
-    elif len(artist) == 0:
-        st.error("Artist mangler")
-    else:
-        current_time = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-        supabase.table("qeuer").insert({"uuid": user_uuid, 
-                                        "name": name,
-                                        "song": song,
-                                        "artist": artist,
-                                        "created_at": current_time}).execute()
-        st.success('Rått! Du vil bli ropt opp når det er din tur!', icon="✅")
-
-# Perform query.
-# Uses st.cache_data to only rerun when the query changes or after 10 min.
-#@st.cache_data(ttl=600)
-#def run_query():
-#    return supabase.table("qeuer").select("*").execute()
-
-#rows = run_query()
-
-# Print results.
-#for row in rows.data:
-#    st.write(row)
+    # Initialize connection.
+    # Uses st.cache_resource to only run once.
+    @st.cache_resource
+    def init_connection():
+        url = st.secrets["API_URL"]
+        key = st.secrets["API_KEY"]
+        return create_client(url, key)
+    
+    supabase = init_connection()
+    
+    #test = supabase.table("qeuer").select("*").execute()
+    st.markdown("Skjema for sangforespørsel")
+    
+    name = st.text_input("Hva er navnet ditt?")
+    song = st.text_input("Hvilken sang vil du spille?")
+    artist = st.text_input("Hva heter artisten?")
+    
+    if st.button("Send inn!"):
+        if len(name) == 0:
+            st.error("Vennligst skriv navnet ditt")
+        
+        elif len(song) == 0:
+            st.error("Sang mangler")
+        
+        elif len(artist) == 0:
+            st.error("Artist mangler")
+        else:
+            current_time = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+            supabase.table("qeuer").insert({"uuid": user_uuid, 
+                                            "name": name,
+                                            "song": song,
+                                            "artist": artist,
+                                            "created_at": current_time}).execute()
+            st.success('Rått! Du vil bli ropt opp når det er din tur!', icon="✅")
+    
+with t2:
+    test = supabase.table("song_list").select("*").execute()
+    col1, col2 = st.columns(2)
+    for el in test.data:
+        col1.metric(el['artist'], el['song'])
+        if col2.box('Velg'):
+            with st.form('Info'):
+                form_name = st.text_input("Hva er navnet ditt?")
+                st.form_submit_button("Send inn!")
+                supabase.table("qeuer").insert({"uuid": user_uuid, 
+                                            "name": form_name,
+                                            "song": el['song'],
+                                            "artist": el['artist'],
+                                            "created_at": current_time}).execute()
+                st.success('Nydelig! Du vil bli ropt opp når det er din tur!', icon="✅")
+                
+        
+                
+    
+    
+    
