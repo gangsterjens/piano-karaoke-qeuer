d45e35dff5542779bdf4b3656525bd4ba0b5b659
Øystein Skjellfjord <90387893+gangsterjens@users.noreply.github.com>
2024-08-30 08:17:22 +0200

    Update list.py

--- list.py
+++ list.py
@@ -20,7 +20,7 @@ if st.button('Last inn:'):
     df['created_at'] = pd.to_datetime(df['created_at'], format='%Y-%m-%dT%H:%M:%S', errors='coerce')
     df = df.sort_values('created_at', ascending=False)
     
-    df = df[['uuid', 'name', 'song', 'artist', 'have_played']])
+    df = df[['uuid', 'name', 'song', 'artist', 'have_played']]
     for index, row in df.iterrows():
         c1, c2 = st.columns([9, 1])
         c1.markdown(f"#### {row['name']} | {row['song']} | {row['artist']}"
