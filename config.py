5aeb8fe164f74a236e755a4d4241d62b4666bf70
Øystein Skjellfjord <90387893+gangsterjens@users.noreply.github.com>
2025-01-07 17:27:20 +0000

    test

--- config.py
+++ config.py
@@ -1,8 +1,3 @@
-secrets = {
-        'API_KEY': 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Ind1bmZlbm1qdG1vY3pjd3dteHhxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjQ5MjYwMzgsImV4cCI6MjA0MDUwMjAzOH0.msCNJTmvMyjmffzFR6AW9BoC_DyqLKSOawGwHSXgy5E',
-        'API_URL': 'https://wunfenmjtmoczcwwmxxq.supabase.co',
-        'client_id': 'a5c01ecb5bd041dfa5df1d5d5f8bdae3',
-        'client_secret': '95a8401c74434a63a3676c928ee3ce86',
-        'martin_playlist': '0pDzkdAe9tD61AWGr4PYFc',
-        'oystein_playlist': '0OPqFGdPJUiM0TgMUALTXg'
-        }
+import os
+os.environ
+secrets = os.getenv("secrets")
