283135f13a9c6169dd37791a9b27c74ad92da8b3
Øystein Skjellfjord <90387893+gangsterjens@users.noreply.github.com>
2025-04-17 16:17:08 +0200

    Update lyric_display.py

--- lyric_display.py
+++ lyric_display.py
@@ -1,16 +1,12 @@
 import streamlit as st
 import streamlit.components.v1 as components
 
-# Streamlit app title
-st.title("Embedding a Web Page in Streamlit")
 
-# URL of the page to embed
-url = st.text_input("enter url here")
+st.input('Skriv inn sang')
 
-# HTML for embedding the page using an iframe
-iframe_html = f"""
-<div id='rg_embed_link_378195' class='rg_embed_link' data-song-id='378195'>Read <a href='https://genius.com/Sia-chandelier-lyrics'>“Chandelier” by Sia</a> on Genius</div> <script crossorigin src='//genius.com/songs/378195/embed.js'></script>
-"""
+st.input('Skriv inn artist')
 
-# Display the iframe in the Streamlit app
-components.html(iframe_html, height=600)
+if st.button('Find lyrics'):
+# Streamlit app ti
+  text = get_g_lyrics(song, artist)
+  st.markdown(text)
